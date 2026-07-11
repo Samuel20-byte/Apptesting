79222b02f1f3ce1deadf671ed0fb0fbb9ce8a85d
Samuel20-byte <samuelkamanda2004@gmail.com>
2026-07-11 13:41:24 +0300

    Solving bug in saving transactions

--- Malihub Backend/src/routes/transactionRoute.js
+++ Malihub Backend/src/routes/transactionRoute.js
@@ -6,4 +6,7 @@ const router = express.Router();
 router.use(auth);
 router.get("/summary", transactionController.summary);
 router.get("/", transactionController.list);
+router.post("/", transactionController.create);
+router.put("/:transactionId", transactionController.update);
+router.delete("/:transactionId", transactionController.remove);
 module.exports = router;
